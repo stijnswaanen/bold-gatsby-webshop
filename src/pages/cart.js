0667bafb5258b7f9263f08629e05a28aa3f94a4e
Stijn Swaanen <swaanenstijn@gmail.com>
2021-11-27 21:17:02 +0100

    exercise ch03

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,11 +1,112 @@
 import React from "react";
-
+import { Row, Col, Table, Divider, Space, Statistic, Button } from "antd";
+import { CreditCardOutlined, DeleteOutlined } from "@ant-design/icons";
+import { useCart } from "../hooks/useCart";
+import { useCartToken } from "../hooks/useCartToken";
 import LayoutWrapper from "../layout/layout-wrapper";
 
 const Cart = () => {
+  const {
+    cartData,
+    removeProductFromCart,
+    removeAllProductsFromCart,
+    resetCart,
+  } = useCart();
+  const { cartToken } = useCartToken();
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Price",
+      key: "price",
+      dataIndex: "price",
+      render: (text, record) => (
+        <Space size="middle">
+          <p>{text}</p>
+        </Space>
+      ),
+    },
+    {
+      title: "Total Price",
+      key: "totalPrice",
+      dataIndex: "totalPrice",
+      render: (text, record) => (
+        <Space size="middle">
+          <p>{text}</p>
+        </Space>
+      ),
+    },
+    {
+      title: "Delete Product",
+      key: "deleteProduct",
+      dataIndex: "deleteProduct",
+      render: (text, record) => (
+        <Space size="middle">
+          <DeleteOutlined
+            onClick={() =>
+              removeProductFromCart({
+                cartToken,
+                orderId: cartData.id,
+                orderItemId: record.id,
+              })
+            }
+          />
+        </Space>
+      ),
+    },
+  ];
   return (
     <LayoutWrapper>
       <h1>Cart</h1>
+      <Row justify="end">
+        <Col>
+          <Button
+            type="default"
+            danger
+            onClick={() =>
+              removeAllProductsFromCart({
+                cartToken,
+                orderId: cartData.id,
+              })
+            }
+          >
+            <DeleteOutlined />
+            &nbsp;
+            <span>Delete Cart</span>
+          </Button>
+        </Col>
+      </Row>
+      <h2>
+        Total Items <strong>({cartData.totalQuantity})</strong>
+      </h2>
+      <br></br>
+      <Table
+        columns={columns}
+        dataSource={cartData.orderItems}
+        pagination={false}
+        rowKey={(record) => record.id}
+      />
+      <Divider orientation="right" />
+      <Row justify="end">
+        <Col>
+          <Statistic title="Total" value={cartData.totalPrice} />
+          <Button
+            style={{ marginTop: 16 }}
+            type="primary"
+            onClick={() => resetCart()}
+          >
+            Pay now <CreditCardOutlined />
+          </Button>
+        </Col>
+      </Row>
     </LayoutWrapper>
   );
 };
---
+++ src/templates/product.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { graphql } from "gatsby";
+import { Button, Dropdown, Menu, Row, Col, Space } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { useCartToken } from "../hooks/useCartToken";
+
+import LayoutWrapper from "../layout/layout-wrapper";
+import { useCart } from "../hooks/useCart";
+
+const Product = ({ data }) => {
+  const {
+    productSweater: {
+      title,
+      body,
+      field_image: { alt: altImage },
+      relationships: {
+        field_image: { localFile },
+        variations,
+      },
+    },
+  } = data;
+  const { addProductToCart, addProductToCartStatus } = useCart();
+  const [activeSize, setActiveSize] = React.useState({
+    id: "",
+    size: "",
+    price: "",
+    type: "",
+  });
+  const { cartToken } = useCartToken();
+
+  // const getSizeLabel = (sizeTitle) => sizeTitle.split(" - ")[1];
+
+  React.useEffect(() => {
+    const defaultSize = variations.find(
+      (product) => product.relationships.attribute_size.name === 'M'
+    );
+    setActiveSize({
+      id: defaultSize.drupal_id,
+      size: defaultSize.relationships.attribute_size.name,
+      price: defaultSize.price.formatted,
+      type: defaultSize.internal.type,
+    });
+  }, []);
+
+  const handleSizeChange = (e) => {
+    const activeProductVariation = variations.find(
+      (product) => product.drupal_id === e.key
+    );
+    setActiveSize({
+      id: e.key,
+      size: activeProductVariation.relationships.attribute_size.name,
+      price: activeProductVariation.price.formatted,
+      type: activeProductVariation.internal.type,
+    });
+  };
+
+  const addItemToCart = () => {
+    addProductToCart({
+      cartToken,
+      activeSize,
+    });
+  };
+
+  const sizeMenu = (
+    <Menu onClick={handleSizeChange}>
+      {variations.map((product) => (
+        <Menu.Item key={product.drupal_id}>{product.relationships.attribute_size.name}</Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  return (
+    <LayoutWrapper>
+      <h1>{title}</h1>
+      <Row>
+        <Col xs={24} md={8}>
+          <GatsbyImage image={getImage(localFile)} alt={altImage} />
+        </Col>
+        <Col xs={24} md={16}>
+          <div dangerouslySetInnerHTML={{ __html: body?.processed }} />
+          <Space>
+            <span><strong>{activeSize.price}</strong></span>
+            <Dropdown
+              overlay={sizeMenu}
+              trigger={["click"]}
+              placement="bottomRight"
+            >
+              <Button>
+                {activeSize.size} <DownOutlined />
+              </Button>
+            </Dropdown>
+            <Button
+              onClick={addItemToCart}
+              type="primary"
+              icon={<ShoppingCartOutlined />}
+            >
+              Add to cart
+            </Button>
+          </Space>
+        </Col>
+      </Row>
+    </LayoutWrapper>
+  );
+};
+
+export default Product;
